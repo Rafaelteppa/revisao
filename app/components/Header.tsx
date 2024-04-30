f2df8c4ff41703abbb43ca2924c609d668a90296
Rafaelteppa <167258743+Rafaelteppa@users.noreply.github.com>
2024-04-30 01:38:04 +0000

    fix: começo

---
+++ app/components/Header.tsx
@@ -0,0 +1,14 @@
+export default function Header(){
+    return(
+        <div className="md:flex justify-between lg:mt-8 ">
+            <h3>🌴Travel</h3>
+        <nav className="space-x-2 mx-4">
+        <a href="#">Home </a>
+        <a href="#">Tour </a>
+        <a href="#">Destination </a>
+        <a href="#">Discounts </a>   
+        </nav>
+    
+        </div>
+    )  
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="bg-white text-black text-center max-w-[1024px] mx-auto">{children}</body>
     </html>
   );
 }
