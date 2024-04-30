735c79a29ac188a0d18755d9baf47217a42b498f
Rafaelteppa <167258743+Rafaelteppa@users.noreply.github.com>
2024-04-30 03:18:49 +0000

    fix: começo

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 
 import Banner from "./components/Banner";
 import Header from "./components/Header";
+import Landscape from "./components/Landscape";
 
 export default function Home() {
   return (
     <div className="bg-white">
       <Header />
       <Banner/>
+      <Landscape/>
+     
     </div>
  
   )
