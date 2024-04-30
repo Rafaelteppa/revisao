0e34f9afb9019ce352dd1db16fa1670f175735c0
Rafaelteppa <167258743+Rafaelteppa@users.noreply.github.com>
2024-04-30 02:02:29 +0000

    fix: começo

---
+++ app/components/Banner.tsx
@@ -0,0 +1,14 @@
+export default function Banner(){
+    return(
+        <div className="mt-3 p-8 text-white bg-orange-600">
+            <h2 className="text-sm">Let's go now</h2>
+            <h3 className="text-base">explore</h3>
+            <h4 className="text-lg font-bold">and travel</h4>
+            <button
+        className="bg-green-700 rounded-md border-lime-600 text-xs text-center p-2 mt-3">
+        VIEW OUR TOURS
+      </button>
+</div>
+
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 
+import Banner from "./components/Banner";
 import Header from "./components/Header";
 
 export default function Home() {
   return (
     <div className="bg-white">
       <Header />
+      <Banner/>
     </div>
  
   )
